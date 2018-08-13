51f8a220337fd5e5d2efc9ddcf7ade245ff54fe4
sampdubs <40965371+sampdubs@users.noreply.github.com>
2018-08-13 19:04:11 -0400

    initial commit

---
+++ info.py
@@ -0,0 +1,15 @@
+import json
+
+data = {}
+
+data['api_key'] = input('What is your api key?')
+data['api_secret'] = input('What is your api secret?')
+data['access_token'] = input('What is your access token?')
+data['access_token_secret'] = input('What is your access token secret?')
+data['twitter_handle'] = input('What is your Twitter handle?')
+data['email'] = input('What is your email?')
+data['password'] = input('What is your email password?')
+data['name'] = input('What is your name?')
+
+with open('config.json', 'w') as f:
+  json.dumps(data, sort_keys=True, indent=2)
